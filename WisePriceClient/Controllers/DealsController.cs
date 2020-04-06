be063317a1b10726e93c3e0d41c1bc61834e0040
Adela Darmansyah <adela.yohana@gmail.com>
2020-04-06 10:12:56 -0700

    initial config for client
    Co-authored-by: Sasa Schwartz <48864808+seschwartz8@users.noreply.github.com>

---
+++ WisePriceClient/Controllers/DealsController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WisePriceClient.Models;
+
+namespace WisePriceClient.Controllers
+{
+    public class DealsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
