20779827a2f200217bacccab19785f702abb6897
Adela Darmansyah <adela.yohana@gmail.com>
2020-04-06 15:17:11 -0700

    Add CRUD methods to Location class, update ApiHelper class

--- WisePriceClient/Models/ApiHelper.cs
+++ WisePriceClient/Models/ApiHelper.cs
@@ -90,5 +90,48 @@ namespace WisePriceClient.Models
       request.AddHeader("Content-Type", "application/json");
       var response = await client.ExecuteTaskAsync(request);
     }
+
+    // LOCATIONS ============================================
+    public static async Task<string> GetAllLocations()
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"locations", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+    public static async Task<string> GetLocation(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"locations/{id}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+    public static async Task PostLocation(string newLocation)
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"locations", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newLocation);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task PutLocation(int id, string locationToEdit)
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"locations/{id}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(locationToEdit);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task DeleteLocation(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"locations/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
+    }
   }
 }
