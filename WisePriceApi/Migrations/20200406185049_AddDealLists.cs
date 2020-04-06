f09f80346259d582af8497e899c3122f2eda8893
Tiffany Siu <tsiu88@gmail.com>
2020-04-06 11:51:57 -0700

    Add migration for deal lists, add controllers
    Co-authored-by: Jiwon Han <jiwon1.han@gmail.com>

---
+++ WisePriceApi/Migrations/20200406185049_AddDealLists.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WisePriceApi.Migrations
+{
+    public partial class AddDealLists : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Deals",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId2",
+                table: "Deals",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deals_UserId1",
+                table: "Deals",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deals_UserId2",
+                table: "Deals",
+                column: "UserId2");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deals_Users_UserId1",
+                table: "Deals",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deals_Users_UserId2",
+                table: "Deals",
+                column: "UserId2",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deals_Users_UserId1",
+                table: "Deals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deals_Users_UserId2",
+                table: "Deals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deals_UserId1",
+                table: "Deals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deals_UserId2",
+                table: "Deals");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Deals");
+
+            migrationBuilder.DropColumn(
+                name: "UserId2",
+                table: "Deals");
+        }
+    }
+}
