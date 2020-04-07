50b98bc1297263566a26e7c39f6cb33894801567
Sarah Schwartz <seschwartz8@gmail.com>
2020-04-07 15:21:38 -0700

    uncomment controller routes

--- WisePriceClient/Models/User.cs
+++ WisePriceClient/Models/User.cs
@@ -1,20 +1,20 @@
-using System.Collections.Generic;
-using System;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+// using System.Collections.Generic;
+// using System;
+// using Newtonsoft.Json;
+// using Newtonsoft.Json.Linq;
 
-namespace WisePriceClient.Models
-{
-  public class User // : IdentityUser ?
-  {
-    public int UserId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string UserName { get; set; }
-    public string Email { get; set; }
-    public string Zip { get; set; }
-    public int Password {get; set;}
-    public virtual ICollection<Deal> PinnedDeals {get; set;}
-    public virtual ICollection<Deal> PostedDeals {get; set;}
-  }
-}
+// namespace WisePriceClient.Models
+// {
+//   public class User // : IdentityUser ?
+//   {
+//     public int UserId { get; set; }
+//     public string FirstName { get; set; }
+//     public string LastName { get; set; }
+//     public string UserName { get; set; }
+//     public string Email { get; set; }
+//     public string Zip { get; set; }
+//     public int Password {get; set;}
+//     public virtual ICollection<Deal> PinnedDeals {get; set;}
+//     public virtual ICollection<Deal> PostedDeals {get; set;}
+//   }
+// }
