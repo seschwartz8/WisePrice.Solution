78ce236bda15386558842936f52a8bc099ecb310
Adela Darmansyah <adela.yohana@gmail.com>
2020-04-09 08:44:08 -0700

    Add PostedDeal API helpers

--- WisePriceClient/Models/PostedDeal.cs
+++ WisePriceClient/Models/PostedDeal.cs
@@ -13,5 +13,39 @@ namespace WisePriceClient.Models
 
     public virtual ApplicationUser User { get; set; }
     public virtual Deal Deal { get; set; }
+
+    // See DEALS for DELETE POSTED DEAL ACTION
+    public static List<PostedDeal> GetAll(string userId)
+    {
+      var apiCallTask = ApiHelper.GetAllPostedDeals(userId);
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<PostedDeal> postedDealList = JsonConvert.DeserializeObject<List<PostedDeal>>(jsonResponse.ToString());
+
+      return postedDealList;
+    }
+
+    public static PinnedDeal Get(string userId, int dealId)
+    {
+      var apiCallTask = ApiHelper.GetPinnedDeal(userId, dealId);
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      PinnedDeal pinnedDeal = JsonConvert.DeserializeObject<PinnedDeal>(jsonResponse.ToString());
+      return pinnedDeal;
+    }
+
+    public static void Post(string userId, PostedDeal newPostedDeal)
+    {
+      string jsonPostedDeal = JsonConvert.SerializeObject(newPostedDeal);
+      var apiCallTask = ApiHelper.PostPostedDeal(userId, jsonPostedDeal);
+    }
+
+    public static int GetCount(string userId)
+    {
+      var apiCallTask = ApiHelper.GetPostedDealCount(userId);
+      var result = int.Parse(apiCallTask.Result);
+      return result;
+    }
   }
 }
